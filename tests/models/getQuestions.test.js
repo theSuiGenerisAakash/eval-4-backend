c80c4f2f5decc85f7569809a8ce1bd3875fc2abc
Aakash Verma <aku06_ran@yahoo.co.in>
2018-02-28 01:39:09 +0530

    getQuestions - Added test for question filled table

--- tests/models/getQuestions.test.js
+++ tests/models/getQuestions.test.js
@@ -1,4 +1,9 @@
 const Server = require('../../src/server');
+const Models = require('../../models');
+
+beforeEach(() => Models.QuesAndAns.destroy({ truncate: true }));
+afterEach(() => Models.QuesAndAns.destroy({ truncate: true }));
+afterAll(() => Models.close());
 
 describe('Testing /questions', () => {
   it('testing with no questions in the table', (done) => {
@@ -9,6 +14,19 @@ describe('Testing /questions', () => {
     Server.inject(request, (response) => {
       expect(response.result.statusCode).toBe(204);
       done();
+    });
+  });
+  it('testing with questions in the table', (done) => {
+    const request = {
+      method: 'GET',
+      url: '/store',
+    };
+    Server.inject(request, (response) => {
+      expect(response.result.statusCode).toBe(201);
+      Models.QuesAndAns.findAll().then((resultOnFind) => {
+        expect(response.result.resultBulkCreate.length).toBe(resultOnFind.length);
+        done();
+      });
     });
   });
 });
