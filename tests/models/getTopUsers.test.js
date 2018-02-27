5ed3ea395211112930b526923bda25e4f874ca95
Aakash Verma <aku06_ran@yahoo.co.in>
2018-02-28 02:01:07 +0530

    getTopUsers - Added test for two users in the table

--- tests/models/getTopUsers.test.js
+++ tests/models/getTopUsers.test.js
@@ -16,4 +16,20 @@ describe('Testing /topusers route', () => {
       done();
     });
   });
+  it('Testing with two user objects inside the table', (done) => {
+    Models.UserScore.bulkCreate([
+      { username: 'Aakash', score: 20 },
+      { username: 'Verma', score: 10 },
+    ]).then(() => {
+      const request = {
+        method: 'GET',
+        url: '/topusers/1',
+      };
+      Server.inject(request, (response) => {
+        expect(response.result.result[0].dataValues.score).toBe(20);
+        expect(response.result.statusCode).toBe(200);
+        done();
+      });
+    });
+  });
 });
