d1f2f0b770bec582dda00bcdedfd6200cd82ed0b
Aakash Verma <aku06_ran@yahoo.co.in>
2018-02-28 01:31:20 +0530

    getUser - Added test with valid user in table

--- tests/models/getUser.test.js
+++ tests/models/getUser.test.js
@@ -16,4 +16,19 @@ describe('Testing /user route', () => {
       done();
     });
   });
+  it('Testing with a valid user objects inside the table', (done) => {
+    Models.UserScore.upsert({
+      username: 'Aakash',
+    }).then(() => {
+      const request = {
+        method: 'GET',
+        url: '/user/Aakash',
+      };
+      Server.inject(request, (response) => {
+        expect(response.result.result.dataValues.username).toBe('Aakash');
+        expect(response.result.statusCode).toBe(200);
+        done();
+      });
+    });
+  });
 });
